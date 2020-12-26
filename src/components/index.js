9bca3f2057db020348c65fefe3566a45f9928e23
Jesús Alberto Jimenez Tapia <jesusjimeneztapia456@gmail.com>
2020-12-25 21:04:50 -0400

    Views structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
-import Form from './Form'
+import Alert from './Alert'
+import Checkbox from './Checkbox'
+import Header from './Header'
+import Popup from './Popup'
 
-export { Form }
+export { Alert, Checkbox, Header, Popup }
