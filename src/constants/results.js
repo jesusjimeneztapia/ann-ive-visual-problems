1fa2dc0f9bc4c36ed72638124e66cb474147229a
Jesús Alberto Jimenez Tapia <jesusjimeneztapia456@gmail.com>
2020-12-28 21:21:35 -0400

    problems

--- src/constants/results.js
+++ src/constants/results.js
@@ -5,19 +5,28 @@ const HYPEROPIA = 'Hipermetropía'
 const ASTIGMATISM = 'Astigmatismo'
 const NORMAL = 'Normal'
 
-const MYOPIA_CHARACTERISTICS = `Características de la ${MYOPIA}.`
+const MYOPIA_CHARACTERISTICS = `la visión lejana de los objetos se torna borroso o distorcionado 
+                                para usted. Los rayos de la luz se enfocan antes de la retina, 
+                                debido a que la forma de su globo ocular se presenta más alargada
+                                horizontalmente, no es totalmente esférica.`
 const HYPEROPIA_CHARACTERISTICS = `la visión cercana de los objetos se torna borroso o distorcionado 
                                 para usted. Los rayos de la luz se enfocan después de la retina, 
                                 debido a que la forma de su globo ocular se presenta más alargada 
                                 verticalmente, no es totalmente esférica.`
-const ASTIGMATISM_CHARACTERISTICS = `Características del ${ASTIGMATISM}.`
-const NORMAL_CHARACTERISTICS = `Características de ${NORMAL}.`
+const ASTIGMATISM_CHARACTERISTICS = `la visión lejana y cercana de los objetos se torna borroso o 
+                                distorcionado para usted. Los rayos de la luz se enfocan en más de 
+                                un punto de la retina, debido a que la córnea no presenta la misma 
+                                curvatura en todas sus zonas.`
+const NORMAL_CHARACTERISTICS = `por lo que no tendrá ningún síntoma referido a la visión borrosa.`
 
 const getTitle = (problem) => {
     return `Ojo ${problem}`
 }
 
 const getEnd = (problem) => {
+    if (problem === NORMAL) {
+        return 'Usted puede observar la comparación entre un ojo normal y los ojos con problemas visuales mediante una gráfica.'
+    }
     return `Usted puede observar la comparación entre un ojo normal y uno con ${problem} mediante una gráfica.`
 }
 
@@ -34,7 +43,9 @@ const getDescription = (problem) => {
             description += `${ASTIGMATISM_CHARACTERISTICS} ${getEnd(ASTIGMATISM)}`
             break
         default:
-            description = `${NORMAL_CHARACTERISTICS} ${getEnd(NORMAL)}`
+            description = `${INIT} un ojo ${problem}, ${NORMAL_CHARACTERISTICS} ${getEnd(
+                NORMAL
+            )}`
     }
     return description
 }
